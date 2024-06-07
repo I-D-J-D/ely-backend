fb13f1a65db7acd2b50ed691235a3e08c30cbf8b
YDongGeun <97018542+YDongGeun@users.noreply.github.com>
2024-06-07 16:00:25 +0900

    init

---
+++ ely/src/main/java/com/github/ely/ElyApplication.java
@@ -0,0 +1,13 @@
+package com.github.ely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElyApplication.class, args);
+	}
+
+}
---
+++ ely/src/test/java/com/github/ely/ElyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
