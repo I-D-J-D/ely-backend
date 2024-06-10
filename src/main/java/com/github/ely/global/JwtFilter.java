3c31be21af6d4094c30a81f8ca0dd047147f4f40
YDongGeun <97018542+YDongGeun@users.noreply.github.com>
2024-06-10 16:46:20 +0900

    update

--- src/main/java/com/github/ely/global/JwtFilter.java
+++ src/main/java/com/github/ely/global/JwtFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,6 +16,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @RequiredArgsConstructor
+@Slf4j
 public class JwtFilter extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
@@ -27,18 +29,18 @@ public class JwtFilter extends OncePerRequestFilter {
 
         //Authorization 헤더 검증
         if (authorization == null || !authorization.startsWith("Bearer ")) {
-            System.out.println("token null");
+            log.info("token null");
             filterChain.doFilter(request, response);
             return;
         }
 
-        System.out.println("authorization now");
+        log.info("authorization now");
         //Bearer 부분 제거 후 순수 토큰만 획득
         String token = authorization.split(" ")[1];
 
         //토큰 소멸 시간 검증
         if (jwtUtil.isExpired(token)) {
-            System.out.println("token expired");
+            log.info("token expired");
             filterChain.doFilter(request, response);
             return;
         }
